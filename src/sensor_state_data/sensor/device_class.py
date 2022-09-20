5fc2703a8cc134e1ef8012e43de1551282b9321e
Ernst Klamer <e.klamer@gmail.com>
2022-09-20 15:31:46 +0200

    Add sensor types that are used by kegtron-ble

--- src/sensor_state_data/sensor/device_class.py
+++ src/sensor_state_data/sensor/device_class.py
@@ -51,6 +51,12 @@ class SensorDeviceClass(BaseDeviceClass):
     # current light level (lx/lm)
     ILLUMINANCE = "illuminance"
 
+    # keg size (L)
+    KEG_SIZE = "keg_size"
+
+    # keg type (no unit)
+    KEG_TYPE = "keg_type"
+
     # % of moisture in the air or soil
     MOISTURE = "moisture"
 
@@ -81,6 +87,15 @@ class SensorDeviceClass(BaseDeviceClass):
     # Particulate matter <= 2.5 μm (µg/m³)
     PM25 = "pm25"
 
+    # Port count (no unit)
+    PORT_COUNT = "port_count"
+
+    # Port name (no unit)
+    PORT_NAME = "port_name"
+
+    # Port state (no unit)
+    PORT_STATE = "port_state"
+
     # power factor (%)
     POWER_FACTOR = "power_factor"
 
@@ -101,6 +116,12 @@ class SensorDeviceClass(BaseDeviceClass):
 
     # Amount of SO2 (µg/m³)
     SULPHUR_DIOXIDE = "sulphur_dioxide"
+
+    # Volume dispensed (L)
+    VOLUME_DISPENSED = "volume_dispensed"
+
+    # Volume start (L)
+    VOLUME_START = "volume_start"
 
     # temperature (°C/F)
     TEMPERATURE = "temperature"
